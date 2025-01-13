3e825fd082707902ef0dff9998166d639da154a8
RokRask <rokas.raskevic@gmail.com>
2025-01-13 19:54:52 +0200

    commit

--- react_forms/src/App.jsx
+++ react_forms/src/App.jsx
@@ -1,26 +1,36 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './buttons.scss';
 import Antras from './Components/Antras';
 import Ketvirtas from './Components/Ketvirtas';
+import Penktas from './Components/Penktas';
 import Pirmas from './Components/Pirmas';
 import Trecias from './Components/Trecias';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      <Pirmas />
-
-      <Antras />
-
-      <Trecias />
-
-      <Ketvirtas />
-
+    <div className="App container py-5">
+      <header className="App-header text-center mb-5">
+        <h1 className="mb-4">React Komponentai</h1>
+        <div className="mb-4">
+          <Pirmas />
+        </div>
+        <div className="mb-4">
+          <Antras />
+        </div>
+        <div className="mb-4">
+          <Trecias />
+        </div>
+        <div className="mb-4">
+          <Ketvirtas />
+        </div>
+        <div className="mb-4">
+          <Penktas />
+        </div>
       </header>
     </div>
   );
 }
 
 export default App;
+
